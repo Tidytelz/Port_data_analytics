562bf10edaa6af2c3f3049dc96004082a927c5a7
Cyril <60722059+Tidytelz@users.noreply.github.com>
2026-01-02 09:04:15 +0000

    Modify New_synthetic_2025 table structure and data load

--- SQL_1.sql
+++ SQL_1.sql
@@ -25,11 +25,11 @@ SET
 DROP TABLE IF EXISTS New_synthetic_2025;
 
 CREATE TABLE New_synthetic_2025 (
-	Date_time VARCHAR(20),
-    Hs_M1 FLOAT,
-    Hs_M2 FLOAT,
-    Hs_M3 FLOAT,
-    Hs_M4 FLOAT
+	dt VARCHAR(20),
+    Hs FLOAT,
+    S1_Hs FLOAT,
+    S2_Hs FLOAT,
+    Ws_Hs FLOAT
 );
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/New_synthetic_2025.csv'
@@ -38,13 +38,13 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS
-(@Date_time, @Hs_M1, @ Hs_M2, @Hs_M3, @Hs_M4)
+(@dt, @Hs, @S1_Hs, @S2_Hs, @Ws_Hs)
 SET
     Date_time = NULLIF(@dt, ''),
-    Hs_M1 = NULLIF(@Hs, ''),
-    Hs_M2 = NULLIF(@S1_Hs, ''),
-    Hs_M3 = NULLIF(@S2_Hs, ''),
-    Hs_M4 = NULLIF(@Ws_Hs, '')
+    Hs = NULLIF(@Hs, ''),
+    S1_Hs = NULLIF(@S1_Hs, ''),
+    S2_Hs = NULLIF(@S2_Hs, ''),
+    Ws_Hs = NULLIF(@Ws_Hs, '')
  ;
  
 
